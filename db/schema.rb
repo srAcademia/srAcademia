cb1cbf470393e49a6256c47434da2845f27b5c9a
KelwinJonas <kelwinjonas@gmail.com>
2020-10-07 10:55:27 -0300

    Adicionando coluna password_digest para autenticacao

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_132502) do
+ActiveRecord::Schema.define(version: 2020_10_07_134426) do
 
   create_table "usuarios", force: :cascade do |t|
     t.string "nome"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_10_07_132502) do
     t.date "data_nascimento"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
