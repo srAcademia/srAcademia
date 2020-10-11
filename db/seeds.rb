1083eeb7e53da215d1ebc164bf38ebdaefa11532
KelwinJonas <kelwinjonas@gmail.com>
2020-10-11 13:58:56 -0300

    Criacao de usuario admin padrao

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Usuario.create(nome: 'admin', email: 'admin@example.com',
+                    telefone: '8799999999', data_nascimento: Date.new(1999, 9, 9),
+                    password: 'password', password_confirmation: 'password', tipo: 2)
