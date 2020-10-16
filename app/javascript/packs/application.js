855bd7bfcbbd4644644d9364390451371c8e5b4b
wtfnath <nathysantos15@outlook.com>
2020-10-16 11:38:29 -0300

    Mudancas requisitadas pelo codacy

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,11 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require ("jquery3")
-require ("popper")
-require ("bootstrap sprockets")
-import 'bootstrap'
-import './stylesheets/application.scss';
+require("jquery3")
+require("popper")
+require("bootstrap sprockets")
+import'bootstrap'
+import'./stylesheets/application.scss';
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
