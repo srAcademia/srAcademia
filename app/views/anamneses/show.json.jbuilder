426fdb211d2c89019a9de7672f13f5a166f5ea10
Milena Macedo <milenaSantosMcd@gmail.com>
2020-10-17 13:09:33 -0300

    Adicionada a Classe Anamnese

---
+++ app/views/anamneses/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.partial! "anamneses/anamnese", anamnese: @anamnese
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :anamneses
   resources :treino_exercicios
   resources :treinos
   resources :exercicios
