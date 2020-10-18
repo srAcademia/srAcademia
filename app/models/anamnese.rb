397dd7c94a778f7230843845058b9ec0b8f799a4
Milena Macedo <milenaSantosMcd@gmail.com>
2020-10-18 18:10:26 -0300

    Adicionado os validates na classe Anamnese

--- app/models/anamnese.rb
+++ app/models/anamnese.rb
@@ -1,3 +1,13 @@
 class Anamnese < ApplicationRecord
     belongs_to :usuario
+    validates :atividade_trabalho, presence: true,  length: {minimum:3}
+    validates :cardiopatia, presence: true,  length: {minimum:3}
+    validates :cirurgia, presence: true,  length: {minimum:3}
+    validates :dor_corporal, presence: true,  length: {minimum:3}
+    validates :lesao, presence: true,  length: {minimum:3}
+    validates :alergia, presence: true,  length: {minimum:3}
+    validates :fumante, presence: true,  length: {minimum:3}
+    validates :alcolotra, presence: true,  length: {minimum:3}
+    validates :atividade_fisica, presence: true,  length: {minimum:3}
+    validates :objetivo,  presence: true,  length: {minimum:3}
 end
