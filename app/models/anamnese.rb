d1965acaf0c741fb0f136c0daac53d8d4157df10
KelwinJonas <kelwinjonas@gmail.com>
2020-10-24 20:48:35 -0300

    Modificando o validates em anamnese

--- app/models/anamnese.rb
+++ app/models/anamnese.rb
@@ -10,5 +10,5 @@ class Anamnese < ApplicationRecord
     validates :alcolotra, presence: true,  length: {minimum:3}
     validates :atividade_fisica, presence: true,  length: {minimum:3}
     validates :objetivo,  presence: true,  length: {minimum:3}
-    validates :usuario_id, presence: true, uniqueness: true
+    validates :usuario_id, uniqueness: true
 end
